cb6ed772df2145d7a649523a84c1151277a4293b
Tomoki Ichikawa <ichikawa.tomoki@moneyforward.co.jp>
2022-03-29 19:33:40 +0900

    [semver:patch] Authenticate with AWS using OIDC and assume a role. (#1)
    * Authenticate with AWS using OIDC and assume a role.
    
    * Update .circleci/config.yml

---
+++ src/scripts/assume_role.sh
@@ -0,0 +1,14 @@
+# SEE: https://awscli.amazonaws.com/v2/documentation/api/latest/reference/sts/assume-role-with-web-identity.html
+credentials=$(aws sts assume-role-with-web-identity \
+  --role-arn "${PARAM_ROLE_ARN}" \
+  --web-identity-token "${CIRCLE_OIDC_TOKEN}" \
+  --role-session-name "CircleCI" \
+  --duration-seconds "${PARAM_DURATION_SECOND}" \
+  --query "Credentials" \
+  --output "json")
+
+{
+  echo "export AWS_ACCESS_KEY_ID=\"$(echo "$credentials" | jq -r '.AccessKeyId')\""
+  echo "export AWS_SECRET_ACCESS_KEY=\"$(echo "$credentials" | jq -r '.SecretAccessKey')\""
+  echo "export AWS_SESSION_TOKEN=\"$(echo "$credentials" | jq -r '.SessionToken')\""
+} >> "${BASH_ENV}"
